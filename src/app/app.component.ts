cb7255cb12467b35ebc047db6474e4122f0e3658
Hafeez <khan.hafeez826@gmail.com>
2018-10-15 17:56:41 +0530

    ► Adding cordova to the project ► Implementing Onesignal notificaiton system ► Implementing  InAppBrowser functionalities to the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,60 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
+  userId: any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.styleLightContent();
       splashScreen.hide();
+
+      var notificationOpenedCallback = (jsonData) => {
+
+        console.log('The user is: ', this.userId);
+        console.log('JSON : ', jsonData);
+
+        if (this.userId) {
+          var notificationObj = {
+            contents: { en: "message body" },
+            include_player_ids: [this.userId]
+          };
+          window["plugins"].OneSignal.postNotification(notificationObj,
+            (successResponse) => {
+              console.log("Notification Post Success:", successResponse);
+            },
+            (failedResponse) => {
+              console.log("Notification Post Failed: ", failedResponse);
+            }
+          );
+        }
+      };
+      var promptForPushNotificationsWithUserResponse = (jsonData) => {
+        console.log('Not a bad thing: ', jsonData);
+        console.log('promptForPushNotificationsWithUserResponse : ' + JSON.stringify(jsonData));
+      };
+      var notificationReceivedCallback = (jsonData) => {
+        const receivedNotification = JSON.stringify(jsonData);
+        console.log('notificationReceivedCallback : ', receivedNotification);
+      };
+      window["plugins"].OneSignal
+        .startInit("dd0460a5-4dbd-4726-abf8-2d0f907eef30", "465187178023")
+        .handleNotificationReceived(notificationReceivedCallback)
+        .handleNotificationOpened(notificationOpenedCallback)
+        .inFocusDisplaying(window["plugins"].OneSignal.OSInFocusDisplayOption.None)
+        .endInit();
+
+      window["plugins"].OneSignal
+        .promptForPushNotificationsWithUserResponse(promptForPushNotificationsWithUserResponse)
+
+      window["plugins"].OneSignal.getIds((ids) => {
+        this.userId = ids.userId;
+      });
+
     });
+
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,84 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
+// import { OneSignal } from '@ionic-native/onesignal';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  notifications: any[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private iab: InAppBrowser) {
+    // constructor(private localNotifications: LocalNotifications, private platform: Platform, public navCtrl: NavController) {
 
+    // this.platform.ready().then((ready) => {
+    //   console.log('Ready or not: ', ready);
+    //   this.localNotifications.getAll().then((notification) => {
+    //     this.notifications = notification
+    //     console.log('All notifications i have are: ', JSON.stringify(notification));
+    //   })
+    // });
+
+
+
+    // window["plugins"].OneSignal.getAll().subscribe((result) => {
+    //   console.log('Can i get a hell ya', result);
+    // })
+    // this.getAllNotifications();
+    // constructor(private oneSignal: OneSignal, public navCtrl: NavController) {
+
+    // this.oneSignal.startInit("dd0460a5-4dbd-4726-abf8-2d0f907eef30", "465187178023");
+    // // this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    // this.oneSignal.handleNotificationReceived().subscribe((results) => {
+    //   // do something when notification is received
+    //   console.log('in the received block', results);
+    // });
+
+    // this.oneSignal.handleNotificationOpened().subscribe(() => {
+    //   // do something when a notification is opened
+    //   console.log('in the opened block');
+    // });
+
+    // this.oneSignal.endInit();
   }
 
+
+  // private getAllNotifications() {
+  //   this.localNotifications.schedule([{
+  //     id: 1,
+  //     text: 'First ILocalNotification',
+  //     data: { secret: 'First One' }
+  //   }, {
+  //     id: 5,
+  //     text: 'Second ILocalNotification',
+  //     data: { secret: 'Second one' }
+  //   }]);
+  // }
+
+
+  goToWebpage() {
+    const inabOptions: InAppBrowserOptions = {
+      location: 'yes',//Or 'no' Whole upper url part
+      hidden: 'no', //Or  'yes' 
+      clearcache: 'yes',
+      clearsessioncache: 'yes',
+      zoom: 'yes',//Android only ,shows browser zoom controls 
+      hardwareback: 'yes',
+      mediaPlaybackRequiresUserAction: 'no',
+      hideurlbar: 'yes',
+      shouldPauseOnSuspend: 'no', //Android only 
+      closebuttoncaption: 'X', //iOS only
+      disallowoverscroll: 'no', //iOS only 
+      toolbar: 'yes', //iOS only 
+      enableViewportScale: 'no', //iOS only 
+      allowInlineMediaPlayback: 'no',//iOS only 
+      presentationstyle: 'pagesheet',//iOS only 
+      fullscreen: 'yes',//Windows only
+    };
+    this.iab.create('https://www.youtube.com/', '_self', inabOptions)
+  }
 }
